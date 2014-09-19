7bcaeb76270ca6848509abc407ad2fa3809eab4f
selenerr06 <selenerr06@hotmail.com>
2014-09-19 18:43:39 -0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,28 +1,20 @@
 
 #/usr/bin/env python
 import unittest
-from Calculator import Calculator
 
-class testCalculator(unittest.TestCase):
+class Calculator(unittest.TestCase):
 
-	def test_suma_2_mas_2(self):
-		cal=Calculator()
-		self.assertEqual(4,cal.suma(2,2))
-		self.assertEqual(0,cal.resta(2,2))
-		self.assertEqual(6,cal.multiplicacion(3,2))
-		self.assertEqual(8,cal.divicion(16,2))
-		self.assertEqual(16,cal.cuadrado(4))
+	def suma(self,num1,num2):
+		return num1+num2
 		
-		self.assertEqual(16,cal.suma(10,6))
-		self.assertEqual(10,cal.resta(8+2))
-		self.assertEqual(32,cal.multiplicacion(4,8))
-		self.assertEqual(6,cal.divicion(66,11))
-		self.assertEqual(64,cal.cuadrado(8))
+	def resta(self,num1,num2):
+		return num1-num2
 		
-		self.assertEqual(65,cal.suma(8,5))
-		self.assertEqual(8,cal.resta(9,2))
-		self.assertEqual(12,cal.multiplicacion(5,7))
-		self.assertEqual(45,cal.divicion(9,5))
-		self.assertEqual(61,cal.cuadrado(6))
-if __name__== "__main__":
-	unittest.main()
+	def divicion(self,num1,num2):
+		return num1/num
+		
+	def cuadrado(self,num1):
+		return num1*num1
+	
+	def multiplicacion(self,num1,num2):
+		return num1+num2
